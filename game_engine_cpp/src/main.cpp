834f7fcc36e483a4c19b6dbf0998ddc158c35b80
guillaume.roux <guillaume.roux@epitech.eu>
2018-03-04 14:21:53 +0100

    collision with border corrected + adding comments to understand the program a bit better

--- game_engine_cpp/src/main.cpp
+++ game_engine_cpp/src/main.cpp
@@ -5,6 +5,7 @@
 
 #define WIDTH 800
 #define HEIGHT 600
+#define CYCLE 100
 
 int main()
 {
@@ -14,18 +15,23 @@ int main()
     int m_time = 0;
     sf::Clock frameClock;
     float speed = 200.f;
-    std::vector<EvilSpaceship> ennemies;
 
+    // Creating Background
     Background  fond;
     Spaceship ship(sf::Vector2f(WIDTH / 2, HEIGHT / 2));
+
+    // Ennemies with different pattern
     EvilSpaceship badguy(sf::Vector2f(850, HEIGHT / 2));
     EvilSpaceship badguy2(sf::Vector2f(800, HEIGHT / 2 - 50));
     EvilSpaceship badguy3(sf::Vector2f(750, HEIGHT / 2 - 100));
 
+    // Music
     gameSound test;
     test.getLvl1().loop(true);
     test.getLvl1().play();
 
+    // List with all the ennemies
+    std::vector<EvilSpaceship> ennemies;
     ennemies.push_back(badguy);
     ennemies.push_back(badguy2);
     ennemies.push_back(badguy3);
@@ -43,6 +49,7 @@ int main()
       }
       sf::Time frameTime = frameClock.restart();
 
+      // Movement manager for the player
       sf::Vector2f movement(0.f, 0.f);
       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
         movement.y -= speed;
@@ -53,26 +60,31 @@ int main()
       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         movement.x += speed;
 
-	  if (ship.getPosition().x < 0.f)
-		  movement.x += speed;
-	  if (ship.getPosition().y < 0.f)
-		  movement.y += speed;
-	  if (ship.getPosition().x > screenDimensions.x)
-		  movement.x -= speed;
-	  if (ship.getPosition().y > screenDimensions.y)
-		  movement.y -= speed;
+      // Collision manager with the border of the screen
+	    if (ship.getPosition().x < 0.f)
+		    movement.x += speed;
+	    if (ship.getPosition().y < 0.f)
+		    movement.y += speed;
+	    if (ship.getPosition().x + ship.getSize().x > screenDimensions.x)
+		    movement.x -= speed;
+	    if (ship.getPosition().y + ship.getSize().y > screenDimensions.y)
+		    movement.y -= speed;
 
 	  ship.display(movement, frameTime);
 
-    if (m_time % 100 == 0)
+    // Ennemy generator, create an ennemy after each cycle of 100
+    if (m_time % CYCLE == 0)
     {
       EvilSpaceship *ennemyGenerator = new EvilSpaceship(sf::Vector2f(850, HEIGHT / 2));
       ennemies.push_back(*ennemyGenerator);
       m_time = 0;
     }
 
+    // Manage movement for the enemies units using their brain function
     for (int i = 0; i < ennemies.size(); i++)
       ennemies[i].brain(100.f, frameTime);
+
+    // Delete out of screen's units
     for (int i = 0; i < ennemies.size(); i++)
     {
       if (ennemies[i].getPosition().x < 0.f)
